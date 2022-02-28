c267300b21dc52f49b4d68cedf317260e7b459c3
summerorange7 <kapitan7016@icloud.com>
2022-02-28 22:28:01 +0900

    最終課題実装前準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start() //turbolinks無効化→JSを正常に動かせる
 require("@rails/activestorage").start()
 require("channels")
 
