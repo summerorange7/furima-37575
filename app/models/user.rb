d85319ca3edceeb72ac8e05d68b66ed3d6ddc287
summerorange7 <kapitan7016@icloud.com>
2022-03-06 23:51:34 +0900

    各種コメント内容変更、テストコード追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        validates :nickname, presence: true
-        validates :familyname, presence: true
-        validates :firstname, presence: true
-        validates :familyname_kana, presence: true
-        validates :firstname_kana, presence: true
-        # validates :email, uniqueness: true ：参考
-        validates :birthday, presence: true 
- 
-        # has_many :items
-        # has_many :buys
+  validates :nickname, presence: true
+  validates :familyname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :familyname_kana, presence: true, format: { with: /\A[ァ-ヶー-]+\z/ }
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー-]+\z/ }
+  # validates :email, uniqueness: true ：参考
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
-        end
+  # has_many :items
+  # has_many :buys
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    familyname            {'test2'}
-    firstname             {'test3'}
-    familyname_kana       {'test4'}
-    firstname_kana        {'test5'}
-    email                 {'test@example'}
-    password              {'000000'}
-    password_confirmation {password}
-    birthday              {'2000/1/1'}
+    nickname              { 'test' }
+    familyname            { '佐藤' }
+    firstname             { '花子' }
+    familyname_kana       { 'サトウ' }
+    firstname_kana        { 'ハナコ' }
+    email                 { 'test@example' }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    birthday              { '2000/1/1' }
   end
 end
