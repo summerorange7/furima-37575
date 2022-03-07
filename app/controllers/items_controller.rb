c6a5c545cb10e12f908b04a0e6c794fbcf5b2d07
summerorange7 <kapitan7016@icloud.com>
2022-03-07 17:05:01 +0900

    newアクションでの画像表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,13 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new #【学習備忘録】newビューファイルで空箱要る為 
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:description, :image).merge(user_id: current_user.id)
   end
 
 end
