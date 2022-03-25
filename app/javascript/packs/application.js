286c9419d1f012f7db6c055e28b3260b9a07c77d
summerorange7 <kapitan7016@icloud.com>
2022-03-25 21:28:53 +0900

    逐次検索機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("../item_price")//自分でjsファイルを作った場合は必ずここへ記述（ないと動作しない）
 require("../order")
 require("../preview")
+require("../tag")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
